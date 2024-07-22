33c3c5ff717822c143b33932cd52e582a057ba94
maoaixiao1314 <m18634441821@163.com>
2024-07-22 16:45:19 +0800

    Optimize event.go and add event_test.go

--- app/event.go
+++ app/event.go
@@ -2,15 +2,11 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/big"
 	"strings"
 	"time"
-
-	// "encoding/json"
-	"fmt"
-	"math/big"
-
-	// store "./contract" // for demo
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -18,182 +14,103 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	// "github.com/treasurenet/x/evm/types"
 	store "github.com/treasurenetprotocol/treasurenet/app/contract"
 )
 
 type EventLog struct {
 	Code int             `json:"code"`
 	Msg  string          `json:"msg"`
-	Data [][]interface{} `json:"data"` // `json:"-"`No serialization
+	Data [][]interface{} `json:"data"`
 }
 
-// type EventLog struct {
-// 	Code int           `json:"code"`
-// 	Msg  string        `json:"msg"`
-// 	Data []interface{} `json:"data"` // `json:"-"`No serialization
-// }
 type EventLogNew struct {
 	Code int             `json:"code"`
 	Msg  string          `json:"msg"`
-	Data [][]interface{} `json:"data"` // `json:"-"`No serialization
+	Data [][]interface{} `json:"data"`
 }
 
-var Even EventLog
-
-var EvenNew EventLogNew
-
 func getLogs(ctx context.Context, start, end int64) {
-	data1 := make([][]interface{}, 0)
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			// Even = new(EventLog)
-			client, err := ethclient.Dial("ws://127.0.0.1:8546")
-			if err != nil {
-				fmt.Println("No listening:", err)
-				EvenNew = EventLogNew{
-					Code: 300,
-					Msg:  "ethclient Listening error",
-					Data: data1,
-				}
-			} else {
-				// eventSignature := []byte("ItemSet(bytes32,bytes32)")
-				// eventSignature := []byte("bidList(address,uint256,uint256)")
-				// dongqi(address indexed account, uint256 indexed amount, uint256 variety);
-				eventSignature := []byte("BidRecord(address,uint256)")
-				hash := crypto.Keccak256Hash(eventSignature)
-				topic := hash.Hex()
-				fmt.Println("Test get log title new", topic)
-				query := ethereum.FilterQuery{
-					FromBlock: big.NewInt(start),
-					ToBlock:   big.NewInt(end),
-					Topics: [][]common.Hash{
-						{
-							hash,
-						},
-					},
-				}
-				fmt.Println("Listening start：", start)
-				fmt.Println("Listening end：", end)
-				logs1, err := client.FilterLogs(context.Background(), query)
-				if err != nil {
-					EvenNew = EventLogNew{
-						Code: 400,
-						Msg:  "FilterLogs Listening error",
-						Data: data1,
-					}
-				}
-				contractAbi, err := abi.JSON(strings.NewReader(string(store.ContractABI)))
-				if err != nil {
-					EvenNew = EventLogNew{
-						Code: 500,
-						Msg:  "contractAbi Listening error",
-						Data: data1,
-					}
-				}
-				fmt.Printf("contractAbi:%+v\n", contractAbi)
-				if len(logs1) == 0 {
-					EvenNew = EventLogNew{
-						Code: 600,
-						Msg:  "Log is empty",
-						Data: data1,
-					}
-				}
-				dst := make([][]interface{}, len(logs1))
-				for index, vLog := range logs1 {
-					LogAbi, err := contractAbi.Unpack("BidRecord", vLog.Data)
-					if err != nil {
-						log.Fatal(err)
-					}
-					dst[index] = LogAbi
-				}
-				EvenNew = EventLogNew{
-					Code: 200,
-					Msg:  "successful",
-					Data: dst,
-				}
-				fmt.Printf("logabi:%+v\n", dst)
+			if err := fetchLogs(ctx, start, end, "BidRecord"); err != nil {
+				log.Println("Error fetching logs:", err)
 			}
+			time.Sleep(200 * time.Millisecond)
 		}
-		time.Sleep(200 * time.Millisecond)
 	}
 }
 
 func getBidStartLogsNew(ctx context.Context, start, end int64) {
-	BidStartdata := make([][]interface{}, 0)
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			client, err := ethclient.Dial("ws://127.0.0.1:8546")
-			if err != nil {
-				fmt.Println("getBidStartLogs No listening:", err)
-				Even = EventLog{
-					Code: 300,
-					Msg:  "getBidStartLogs ethclient Listening error",
-					Data: BidStartdata,
-				}
-			} else {
-				eventSignature := []byte("BidStart(uint256)")
-				hash := crypto.Keccak256Hash(eventSignature)
-				topic := hash.Hex()
-				fmt.Println("getBidStartLogs title new", topic)
-				query := ethereum.FilterQuery{
-					FromBlock: big.NewInt(start),
-					ToBlock:   big.NewInt(end),
-					Topics: [][]common.Hash{
-						{
-							hash,
-						},
-					},
-				}
-				fmt.Println("getBidStartLogs Listening start：", start)
-				fmt.Println("getBidStartLogs Listening end：", end)
-				logs1, err := client.FilterLogs(context.Background(), query)
-				// client.Close()
-				if err != nil {
-					Even = EventLog{
-						Code: 400,
-						Msg:  "getBidStartLogs FilterLogs Listening error",
-						Data: BidStartdata,
-					}
-				}
-				contractAbi, err := abi.JSON(strings.NewReader(string(store.ContractABI)))
-				if err != nil {
-					Even = EventLog{
-						Code: 500,
-						Msg:  "getBidStartLogs contractAbi Listening error",
-						Data: BidStartdata,
-					}
-				}
-				fmt.Printf("getBidStartLogs contractAbi:%+v\n", contractAbi)
-				if len(logs1) == 0 {
-					Even = EventLog{
-						Code: 600,
-						Msg:  "getBidStartLogs is empty",
-						Data: BidStartdata,
-					}
-				}
-				dst := make([][]interface{}, len(logs1))
-				for index, vLog := range logs1 {
-					LogAbi, err := contractAbi.Unpack("BidStart", vLog.Data)
-					if err != nil {
-						log.Fatal(err)
-					}
-					dst[index] = LogAbi
-				}
-				Even = EventLog{
-					Code: 200,
-					Msg:  "successful",
-					Data: dst,
-				}
-				fmt.Printf("logabi:%+v\n", dst)
+			if err := fetchLogs(ctx, start, end, "BidStart"); err != nil {
+				log.Println("Error fetching BidStart logs:", err)
 			}
+			time.Sleep(200 * time.Millisecond)
 		}
-		time.Sleep(200 * time.Millisecond)
 	}
+}
+
+func fetchLogs(ctx context.Context, start, end int64, eventName string) error {
+	client, err := ethclient.Dial("ws://127.0.0.1:8546")
+	if err != nil {
+		return fmt.Errorf("ethclient Listening error: %w", err)
+	}
+	defer client.Close()
+
+	eventSignature := []byte(eventName)
+	hash := crypto.Keccak256Hash(eventSignature)
+	topic := hash.Hex()
+	fmt.Println("Event title new", eventName, topic)
+
+	query := ethereum.FilterQuery{
+		FromBlock: big.NewInt(start),
+		ToBlock:   big.NewInt(end),
+		Topics: [][]common.Hash{
+			{
+				hash,
+			},
+		},
+	}
+	fmt.Println(eventName, "Listening start：", start)
+	fmt.Println(eventName, "Listening end：", end)
+
+	logs, err := client.FilterLogs(ctx, query)
+	if err != nil {
+		return fmt.Errorf("%s FilterLogs Listening error: %w", eventName, err)
+	}
+
+	contractAbi, err := abi.JSON(strings.NewReader(string(store.ContractABI)))
+	if err != nil {
+		return fmt.Errorf("%s contractAbi Listening error: %w", eventName, err)
+	}
+	fmt.Printf("%s contractAbi:%+v\n", eventName, contractAbi)
+
+	if len(logs) == 0 {
+		return fmt.Errorf("%s Log is empty", eventName)
+	}
+
+	dst := make([][]interface{}, len(logs))
+	for index, vLog := range logs {
+		LogAbi, err := contractAbi.Unpack(eventName, vLog.Data)
+		if err != nil {
+			return fmt.Errorf("unpacking %s log data error: %w", eventName, err)
+		}
+		dst[index] = LogAbi
+	}
+
+	eventLog := EventLog{
+		Code: 200,
+		Msg:  "successful",
+		Data: dst,
+	}
+	fmt.Printf("%s logabi:%+v\n", eventName, dst)
+
+	return nil
 }
