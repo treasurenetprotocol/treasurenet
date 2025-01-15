986781ba47ab361ba450953db9710c9cb06102a6
Jamnly <1027375133@qq.com>
2025-01-15 09:14:04 +0800

    test for workflow

--- init_node_template.sh
+++ init_node_template.sh
@@ -1,47 +1,86 @@
 #!/bin/bash
 set -eux
 
-NODE_HOME=$1
-MONIKER=$2
-CHAIN_ID=$3
-KEYRING=$4
-KEY1=$5
-KEY2=$6
-ALLOCATION=$7
-BIN=$8
-KEYALGO=$9
-LOGLEVEL=${10}
+# Clear any previous data (make sure to do this carefully)
+rm -rf {{DATA_PATH}}/*
+rm -rf {{DATA_PATH}}/.*
+rm -rf {{HOME_PATH}}/*  # 清理默认目录
 
+# Ensure necessary directories exist
+mkdir -p {{DATA_PATH}}/{{PROJECT_NAME}}
+mkdir -p {{DATA_PATH}}/.{{PROJECT_NAME}}
+ sudo chown -R $USER:$USER {{DATA_PATH}}/{{PROJECT_NAME}}
+ sudo chown -R $USER:$USER {{DATA_PATH}}/.{{PROJECT_NAME}}
+# your gaiad binary name
+BIN={{BINARY_NAME}}
+
+CHAIN_ID="{{CHAIN_ID}}"
+ALLOCATION="{{ALLOCATION}}"
+KEY1="{{VALIDATOR_KEY}}"
+KEY2="{{ORCHESTRATOR_KEY}}"
+MONIKER="{{MONIKER}}"
+KEYRING="{{KEYRING}}"
+KEYALGO="{{KEYALGO}}"
+LOGLEVEL="info"
+TRACE="--trace"
+
+# validate dependencies are installed
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
 $BIN config keyring-backend $KEYRING
-$BIN config chain-id $CHAIN_ID --home $NODE_HOME
-$BIN eth_keys add --keyring-backend $KEYRING >> $NODE_HOME/validator0-eth-keys
+$BIN config chain-id $CHAINID
 
-$BIN init $MONIKER --chain-id $CHAIN_ID --keyring-backend $KEYRING --home $NODE_HOME
+# Define home directory for current user
+GAIA_HOME="--home {{HOME_PATH}}"
+ARGS="$GAIA_HOME --keyring-backend test"
 
-GENESIS_JSON="${NODE_HOME}/config/genesis.json"
-cat $GENESIS_JSON | jq '.app_state["staking"]["params"]["bond_denom"]="aunit"' > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json $GENESIS_JSON
-cat $GENESIS_JSON | jq '.app_state["crisis"]["constant_fee"]["denom"]="aunit"' > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json $GENESIS_JSON
-cat $GENESIS_JSON | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aunit"' > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json $GENESIS_JSON
-cat $GENESIS_JSON | jq '.app_state["mint"]["params"]["mint_denom"]="aunit"' > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json $GENESIS_JSON
+# Add keys for validator and orchestrator
+$BIN keys add $KEY1 --keyring-backend $KEYRING --algo $KEYALGO 2>> {{DATA_PATH}}/{{PROJECT_NAME}}/validator0-phrases
+$BIN keys add $KEY2 --keyring-backend $KEYRING --algo $KEYALGO 2>> {{DATA_PATH}}/{{PROJECT_NAME}}/orchestrator0-phrases
+$BIN eth_keys add --keyring-backend $KEYRING >> {{DATA_PATH}}/{{PROJECT_NAME}}/validator0-eth-keys
 
+# Initialize the node
+$BIN init $MONIKER --chain-id $CHAINID 
+
+# Modify genesis.json using jq
+cat {{HOME_PATH}}/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="{{DENOM}}"'> {{HOME_PATH}}/config/tmp_genesis.json && mv {{HOME_PATH}}/config/tmp_genesis.json {{HOME_PATH}}/config/genesis.json
+cat {{HOME_PATH}}/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="{{DENOM}}"'> {{HOME_PATH}}/config/tmp_genesis.json && mv {{HOME_PATH}}/config/tmp_genesis.json {{HOME_PATH}}/config/genesis.json
+cat {{HOME_PATH}}/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="{{DENOM}}"'> {{HOME_PATH}}/config/tmp_genesis.json && mv {{HOME_PATH}}/config/tmp_genesis.json {{HOME_PATH}}/config/genesis.json
+cat {{HOME_PATH}}/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="{{DENOM}}"'> {{HOME_PATH}}/config/tmp_genesis.json && mv {{HOME_PATH}}/config/tmp_genesis.json {{HOME_PATH}}/config/genesis.json
+
+# Modify consensus params in genesis.json
+cat {{HOME_PATH}}/config/genesis.json | jq '.consensus_params["block"]["time_iota_ms"]="1000"' > {{HOME_PATH}}/config/tmp_genesis.json && mv {{HOME_PATH}}/config/tmp_genesis.json {{HOME_PATH}}/config/genesis.json
+cat {{HOME_PATH}}/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="1000000000000"' > {{HOME_PATH}}/config/tmp_genesis.json && mv {{HOME_PATH}}/config/tmp_genesis.json {{HOME_PATH}}/config/genesis.json
+
+# Update the config.toml to disable empty block creation
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' ${NODE_HOME}/config/config.toml
+    sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' {{HOME_PATH}}/config/config.toml
 else
-  sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' ${NODE_HOME}/config/config.toml
+    sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' {{HOME_PATH}}/config/config.toml
 fi
 
-jq '.app_state.bank.denom_metadata += [{"name": "Foo Token", "symbol": "FOO", "base": "footoken", "display": "mfootoken", "description": "A non-staking test token", "denom_units": [{"denom": "footoken", "exponent": 0}, {"denom": "mfootoken", "exponent": 6}]},{"name": "Stake Token", "symbol": "STEAK", "base": "aunit", "display": "unit", "description": "A staking test token", "denom_units": [{"denom": "aunit", "exponent": 0}, {"denom": "unit", "exponent": 18}]}]' ${NODE_HOME}/config/genesis.json > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json $GENESIS_JSON
+# Modify genesis.json for new tokens
+jq '.app_state.bank.denom_metadata += [{"name": "Foo Token", "symbol": "FOO", "base": "footoken", display: "mfootoken", "description": "A non-staking test token", "denom_units": [{"denom": "footoken", "exponent": 0}, {"denom": "mfootoken", "exponent": 6}]},{"name": "Stake Token", "symbol": "UNIT", "base": "aunit", display: "unit", "description": "A staking test token", "denom_units": [{"denom": "aunit", "exponent": 0}, {"denom": "unit", "exponent": 18}]}]' {{HOME_PATH}}/config/genesis.json > /tmp/treasurenet-footoken2-genesis.json
+jq '.app_state.bank.denom_metadata += [{"name": "Foo Token2", "symbol": "F20", "base": "footoken2", display: "mfootoken2", "description": "A second non-staking test token", "denom_units": [{"denom": "footoken2", "exponent": 0}, {"denom": "mfootoken2", "exponent": 6}]}]' /tmp/treasurenet-footoken2-genesis.json > /tmp/treasurenet-bech32ibc-genesis.json
 
-jq '.app_state.bank.denom_metadata += [{"name": "Foo Token2", "symbol": "F20", "base": "footoken2", "display": "mfootoken2", "description": "A second non-staking test token", "denom_units": [{"denom": "footoken2", "exponent": 0}, {"denom": "mfootoken2", "exponent": 6}]}]' ${NODE_HOME}/config/genesis.json > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json ${NODE_HOME}/config/genesis.json
+jq '.app_state.bech32ibc.nativeHRP = "treasurenet"' /tmp/treasurenet-bech32ibc-genesis.json > /tmp/gov-genesis.json
 
-jq '.app_state.bech32ibc.nativeHRP = "treasurenet"' ${NODE_HOME}/config/genesis.json > ${NODE_HOME}/config/tmp_genesis.json && mv ${NODE_HOME}/config/tmp_genesis.json ${NODE_HOME}/config/genesis.json
+# Move final genesis.json back to correct location
+mv /tmp/gov-genesis.json {{HOME_PATH}}/config/genesis.json
 
-ETHEREUM_KEY=$(grep address $NODE_HOME/validator0-eth-keys | sed -n "1"p | sed 's/.*://')
+# Add accounts to genesis
+VALIDATOR_KEY=$($BIN keys show validator0 -a $ARGS)
+ORCHESTRATOR_KEY=$($BIN keys show orchestrator0 -a $ARGS)
+$BIN add-genesis-account $ARGS $VALIDATOR_KEY $ALLOCATION
+$BIN add-genesis-account $ARGS $ORCHESTRATOR_KEY $ALLOCATION
+
+# Generate transaction
+ETHEREUM_KEY=$(grep address {{DATA_PATH}}/{{PROJECT_NAME}}/validator0-eth-keys | sed -n "1"p | sed 's/.*://')
 echo $ETHEREUM_KEY
 
-$BIN gentx --keyring-backend $KEYRING --home $NODE_HOME --moniker $MONIKER --chain-id=$CHAIN_ID $KEY1 200000000000000000000aunit $ETHEREUM_KEY
-$BIN collect-gentxs --home $NODE_HOME
+$BIN gentx $ARGS --moniker $MONIKER --chain-id=$CHAIN_ID validator0 258000000000000000000aunit $ETHEREUM_KEY $ORCHESTRATOR_KEY
 
-echo "Node initialization complete, all transactions collected."
+# Collect transactions
+$BIN collect-gentxs
+
+sudo mv {{HOME_PATH}}/* {{DATA_PATH}}/.{{PROJECT_NAME}}/
--- init_nodes.sh
+++ init_nodes.sh
@@ -1,25 +1,59 @@
 #!/bin/bash
-set -eux
 
+# Load variables from JSON file
+JSON_FILE="variables.json"
+TEMPLATE_FILE="template.sh"
 
-CONFIG_FILE="node_config.json"
-NODES=$(jq -c '.nodes[]' $CONFIG_FILE)
+# Ensure jq is installed
+command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
-echo "Cleaning up previous files..."
-rm -rf /data/node/* 
+# Read JSON and iterate over each object
+for ((i=0; i<$(jq length $JSON_FILE); i++)); do
+  # Extract variables for this iteration
+  DATA_PATH=$(jq -r ".[$i].DATA_PATH" $JSON_FILE)
+  HOME_PATH=$(jq -r ".[$i].HOME_PATH" $JSON_FILE)
+  PROJECT_NAME=$(jq -r ".[$i].PROJECT_NAME" $JSON_FILE)
+  BINARY_NAME=$(jq -r ".[$i].BINARY_NAME" $JSON_FILE)
+  CHAIN_ID=$(jq -r ".[$i].CHAIN_ID" $JSON_FILE)
+  ALLOCATION=$(jq -r ".[$i].ALLOCATION" $JSON_FILE)
+  VALIDATOR_KEY=$(jq -r ".[$i].VALIDATOR_KEY" $JSON_FILE)
+  ORCHESTRATOR_KEY=$(jq -r ".[$i].ORCHESTRATOR_KEY" $JSON_FILE)
+  MONIKER=$(jq -r ".[$i].MONIKER" $JSON_FILE)
+  KEYRING=$(jq -r ".[$i].KEYRING" $JSON_FILE)
+  KEYALGO=$(jq -r ".[$i].KEYALGO" $JSON_FILE)
+  DENOM=$(jq -r ".[$i].DENOM" $JSON_FILE)
+  NODE_NAME=$(jq -r ".[$i].NODE_NAME" $JSON_FILE)
 
-for NODE in $NODES; do
-  NODE_HOME=$(echo $NODE | jq -r '.NODE_HOME')
-  MONIKER=$(echo $NODE | jq -r '.MONIKER')  
-  CHAIN_ID=$(echo $NODE | jq -r '.CHAIN_ID')
-  KEYRING=$(echo $NODE | jq -r '.KEYRING')
-  KEY1=$(echo $NODE | jq -r '.KEY1')
-  KEY2=$(echo $NODE | jq -r '.KEY2')
-  ALLOCATION=$(echo $NODE | jq -r '.ALLOCATION')
-  BIN=$(echo $NODE | jq -r '.BIN')
-  KEYALGO=$(echo $NODE | jq -r '.KEYALGO')  
-  LOGLEVEL=$(echo $NODE | jq -r '.LOGLEVEL') 
+  # Create a temporary environment file for this iteration
+  ENV_FILE=$(mktemp)
+  cat <<EOF > $ENV_FILE
+export DATA_PATH=$DATA_PATH
+export HOME_PATH=$HOME_PATH
+export PROJECT_NAME=$PROJECT_NAME
+export BINARY_NAME=$BINARY_NAME
+export CHAIN_ID=$CHAIN_ID
+export ALLOCATION=$ALLOCATION
+export VALIDATOR_KEY=$VALIDATOR_KEY
+export ORCHESTRATOR_KEY=$ORCHESTRATOR_KEY
+export MONIKER=$MONIKER
+export KEYRING=$KEYRING
+export KEYALGO=$KEYALGO
+export DENOM=$DENOM
+export NODE_NAME=$NODE_NAME
+EOF
 
-  # 执行模板脚本，传递参数
-  ./init_node_template.sh $NODE_HOME $MONIKER $CHAIN_ID $KEYRING $KEY1 $KEY2 $ALLOCATION $BIN $KEYALGO $LOGLEVEL
+  # Source the environment variables into the current shell session
+  source $ENV_FILE
+
+  # Read the template file and replace placeholders with actual values using envsubst
+  envsubst < $TEMPLATE_FILE > run_$NODE_NAME.sh
+
+  # Make the generated script executable
+  chmod +x run_$NODE_NAME.sh
+
+  # Optionally, you can execute the generated script here:
+  # ./run_$NODE_NAME.sh
+
+  # Clean up temporary environment file
+  rm $ENV_FILE
 done
