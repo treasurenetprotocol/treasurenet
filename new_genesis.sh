f7f791164d4b2f91e9fcad347bb395e0bfe3b4f2
Jamnly <1027375133@qq.com>
2025-01-15 17:09:43 +0800

    test for workflow

--- new_genesis.sh
+++ new_genesis.sh
@@ -93,17 +93,17 @@ jq --argjson accounts00 "$accounts00" --argjson accounts01 "$accounts01" \
    )
 ' template.json > genesis.json
 
-cp -a genesis.json /data/node0/.treasurenet/config/genesis.json
-cp -a genesis.json /data/node1/.treasurenet/config/genesis.json
-cp -a genesis.json /data/node2/.treasurenet/config/genesis.json
-cp -a genesis.json /data/node3/.treasurenet/config/genesis.json
+cp -a genesis.json /data/node0/.treasurenetd/config/genesis.json
+cp -a genesis.json /data/node1/.treasurenetd/config/genesis.json
+cp -a genesis.json /data/node2/.treasurenetd/config/genesis.json
+cp -a genesis.json /data/node3/.treasurenetd/config/genesis.json
 sudo mkdir -p /data/node/gen_txs
  sudo chown -R $USER:$USER /data/node
- cp -a /data/node0/.treasurenet/config/gentx/ /data/node/gen_txs/
- cp -a /data/node1/.treasurenet/config/gentx/ /data/node/gen_txs/
- cp -a /data/node2/.treasurenet/config/gentx/ /data/node/gen_txs/
- cp -a /data/node3/.treasurenet/config/gentx/ /data/node/gen_txs/
- cp -a /data/node/gen_txs/ /data/node0/.treasurenet/config/gentx/
- cp -a /data/node/gen_txs/ /data/node1/.treasurenet/config/gentx/
- cp -a /data/node/gen_txs/ /data/node2/.treasurenet/config/gentx/
- cp -a /data/node/gen_txs/ /data/node3/.treasurenet/config/gentx/
+ cp -a /data/node0/.treasurenetd/config/gentx/ /data/node/gen_txs/
+ cp -a /data/node1/.treasurenetd/config/gentx/ /data/node/gen_txs/
+ cp -a /data/node2/.treasurenetd/config/gentx/ /data/node/gen_txs/
+ cp -a /data/node3/.treasurenetd/config/gentx/ /data/node/gen_txs/
+ cp -a /data/node/gen_txs/ /data/node0/.treasurenetd/config/gentx/
+ cp -a /data/node/gen_txs/ /data/node1/.treasurenetd/config/gentx/
+ cp -a /data/node/gen_txs/ /data/node2/.treasurenetd/config/gentx/
+ cp -a /data/node/gen_txs/ /data/node3/.treasurenetd/config/gentx/
