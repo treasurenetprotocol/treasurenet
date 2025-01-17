5fd0f157b81238565f0b5471bd1030ef71e30d39
Jamnly <1027375133@qq.com>
2025-01-17 09:16:05 +0800

    test for workflow

--- init_node_template.sh
+++ init_node_template.sh
@@ -25,6 +25,7 @@ KEYALGO="$KEYALGO"
 LOGLEVEL="info"
 TRACE="--trace"
 
+
 # validate dependencies are installed
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
