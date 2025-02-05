22b8648f111f2f5f7232496db4073f1e56cf7ec2
Jamnly <1027375133@qq.com>
2025-02-05 14:50:26 +0800

    test for workflow

--- init-gravity.sh
+++ init-gravity.sh
@@ -37,7 +37,7 @@ command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https
 # remove existing daemon and client
 rm -rf ~/.treasurenet*
 
-make install
+
 
 $BIN config keyring-backend $KEYRING
 $BIN config chain-id $CHAINID
