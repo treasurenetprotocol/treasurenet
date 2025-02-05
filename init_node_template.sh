d89ed4c696d4fb29c36adcb6fd76a7394740cba3
Jamnly <1027375133@qq.com>
2025-02-05 16:38:45 +0800

    test

--- init_node_template.sh
+++ init_node_template.sh
@@ -77,18 +77,10 @@ $BIN add-genesis-account $ARGS $VALIDATOR_KEY1 $ALLOCATION
 $BIN add-genesis-account $ARGS $ORCHESTRATOR_KEY1 $ALLOCATION
 
 FILE="/data/test.json"
-# sudo rm -f $FILE
 
-if [ ! -f "$FILE" ]; then
-
-# sudo touch $FILE
-# sudo chmod 666 /data/test.json
-
-else
- 
   sudo jq --arg key1 "$KEY1" --arg key2 "$KEY2" --arg validator_key "$VALIDATOR_KEY1" --arg orchestrator_key "$ORCHESTRATOR_KEY1" \
     '. + {($key1): $validator_key, ($key2): $orchestrator_key}' "$FILE" > tmp.json && sudo mv tmp.json "$FILE"
-fi
+
 
 # Generate transaction
 ETHEREUM_KEY=$(grep address $DATA_PATH/.$PROJECT_NAME/$KEY1-eth-keys | sed -n "1"p | sed 's/.*://')
