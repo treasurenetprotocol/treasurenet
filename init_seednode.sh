1fafbc367d944befcf497ddce15e861c234ca83b
Jamnly <1027375133@qq.com>
2025-02-11 15:12:34 +0800

    add seed node test for workflow

--- init_seednode.sh
+++ init_seednode.sh
@@ -1,8 +1,12 @@
-rm -rf /home/ubuntu/.treasurenetd
+sudo rm -rf /home/ubuntu/.treasurenetd
 
 treasurenetd init seednode --chain-id "treasurenet_5005-1"
 
-mv /home/ubuntu/.treasurenetd  /data/seednode/.treasurenetd/config/genesis.json
+sudo mkdir -p /data/seednode/.treasurenetd
+
+sudo rm -rf /data/seednode/.treasurenetd
+
+sudo mv /home/ubuntu/.treasurenetd  /data/seednode/.treasurenetd
 
 cd /data/node1/.treasurenetd/config/
 
