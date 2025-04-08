d9bcd3b80b9f3094cd1e6c0cf4d4e7509df467b2
Jamnly <1027375133@qq.com>
2025-04-08 16:46:35 +0800

    update init

--- new_genesis.sh
+++ new_genesis.sh
@@ -20,7 +20,7 @@ for key in $(jq -r 'keys_unsorted[]' "$json_file"); do
     ACCOUNT=$(jq -r ".${key}" "$json_file")
     echo "Adding genesis account for $key with address $ACCOUNT"
     
-    treasurenetd add-genesis-account --trace --keyring-backend test $ACCOUNT 10000000000000000000000aunit,10000000000stake,10000000000footoken,10000000000footoken2,10000000000ibc/nometadatatoken
+    printf "your_password\n" | treasurenetd add-genesis-account --trace --keyring-backend file $ACCOUNT 10000000000000000000000aunit,10000000000stake,10000000000footoken,10000000000footoken2,10000000000ibc/nometadatatoken
   fi
 done
 
