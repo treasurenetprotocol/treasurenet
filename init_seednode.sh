5ac0ba36953f1c0a7c32cc38a0e4575c1fd8fd16
Jamnly <1027375133@qq.com>
2025-04-08 13:39:23 +0800

    update workflow

--- init_seednode.sh
+++ init_seednode.sh
@@ -25,3 +25,16 @@ sudo mv /home/ubuntu/.treasurenetd  /data/bootnode-2/.treasurenetd
 cd /data/genesis-validator-1/.treasurenetd/config/
 
 cp -a genesis.json /data/bootnode-2/.treasurenetd/config/genesis.json
+
+# 定义节点列表
+nodes=("bootnode-1" "bootnode-2")
+
+# 记录节点 ID 到 .env 文件
+for node in "${nodes[@]}"; do
+  export HOME="/data/$node"
+  node_id=$(treasurenetd tendermint show-node-id)
+  echo "${node}_address=$node_id" >> /data/actions-runner/_work/treasurenet/treasurenet/.github/scripts/ansible/docker/.env
+done
+
+# 恢复 HOME 环境变量
+export HOME=/home/ubuntu
