a0b9f0580ef6fe5c5062259105a8d65c6a4d8f71
Jamnly <1027375133@qq.com>
2025-04-11 10:55:28 +0800

    Delete useless files

--- .github/scripts/ansible/docker/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eux
-$BIN keys add $KEY1 --keyring-backend $KEYRING --algo $KEYALGO 2>> /data/treasurenet/validator0-phrases
-$BIN keys add $KEY2 --keyring-backend $KEYRING --algo $KEYALGO 2>> /data/treasurenet/orchestrator0-phrases
-$BIN eth_keys add --keyring-backend $KEYRING >> /data/treasurenet/validator0-eth-keys
