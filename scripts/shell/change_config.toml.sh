4b760fe9206e8d8909cad692e8f80a8acde9a023
Jamnly <1027375133@qq.com>
2025-04-18 09:56:49 +0800

    update sed logic

--- scripts/shell/change_config.toml.sh
+++ scripts/shell/change_config.toml.sh
@@ -21,6 +21,7 @@
 input_file="config.toml"
 output_file="modified_config.toml"
 
+
 # Process all target nodes
 for node in genesis-validator-{1..6} bootnode-{1..2} rpc-{1..2}; do
     # Node configuration directory
@@ -40,7 +41,7 @@ for node in genesis-validator-{1..6} bootnode-{1..2} rpc-{1..2}; do
         -e 's|^(laddr[[:space:]]*=[[:space:]]*"tcp://)127.0.0.1:26657"|\10.0.0.0:26657"|' \
         "$input_file" > "$output_file"
     if [[ $node == bootnode* ]]; then
-        sed -Ei '' '/^addr_book_strict[[:space:]]*=[[:space:]]*true$/s/true/false/' "$output_file"
+        sed -E -e  '/^addr_book_strict[[:space:]]*=[[:space:]]*true/s/true/false/' "$output_file" 
         echo " $node Turn off address book strict mode"
     fi
     # Safely replace original config file
